d5cee240a71729ecc808d471d53d7ca83bef669c
Marcelo Da Ros <marcelodaros94@gmail.com>
2021-08-29 11:41:19 -0500

    form

--- quizapp/routes/web.php
+++ quizapp/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/paso-1', function () {
+    return view('paso1');
+});
+Route::resource('/landing', 'LandingController');
